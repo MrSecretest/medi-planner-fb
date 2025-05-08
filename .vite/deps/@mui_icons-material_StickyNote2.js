2c8dcb30c99c419c04f7ff9e1270540159edb2be
olehkulys <kulys.olezik@gmail.com>
2025-05-08 21:42:27 +0300

    path changed

---
+++ .vite/deps/@mui_icons-material_StickyNote2.js
@@ -0,0 +1,22 @@
+"use client";
+import {
+  createSvgIcon
+} from "./chunk-E5CUOQYA.js";
+import "./chunk-EZSJO6EY.js";
+import {
+  require_jsx_runtime
+} from "./chunk-6LX6CQ4K.js";
+import "./chunk-W4EHDCLL.js";
+import {
+  __toESM
+} from "./chunk-EWTE5DHJ.js";
+
+// node_modules/@mui/icons-material/esm/StickyNote2.js
+var import_jsx_runtime = __toESM(require_jsx_runtime());
+var StickyNote2_default = createSvgIcon((0, import_jsx_runtime.jsx)("path", {
+  d: "M19 3H4.99C3.89 3 3 3.9 3 5l.01 14c0 1.1.89 2 1.99 2h10l6-6V5c0-1.1-.9-2-2-2M7 8h10v2H7zm5 6H7v-2h5zm2 5.5V14h5.5z"
+}), "StickyNote2");
+export {
+  StickyNote2_default as default
+};
+//# sourceMappingURL=@mui_icons-material_StickyNote2.js.map
